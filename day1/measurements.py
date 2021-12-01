b37ec54dead8f4f289c067a93f08f18fb70c6fbc
unknown <kkols94@gmail.com>
2021-12-01 20:26:42 +0200

    Fixed code and cleaned up a bit

--- day1/measurements.py
+++ day1/measurements.py
@@ -1,5 +1,7 @@
 from sys import exit
 
+DEPTH = 3
+
 def read_file_to_list(fname: str, encoding='utf-8') -> list[str]:
     data = []
     with open(fname, 'r', encoding=encoding) as file:
@@ -15,28 +17,17 @@ def list_to_int_list(data: list) -> list[int]:
         exit('Unable to convert list to type int.\nData sample {}\nQuitting..'.format(data_sample))
 
 def count_depth_increments(measurements: list[int], depth=1) -> int:
-    def list_to_buckets(data: list, bucket_size: int) -> list:
-        for i in range(0, len(data)):
-            bucket = data[i:i+bucket_size]
-            if len(bucket) == bucket_size:
-                yield sum(bucket)
-    
     if depth < 1:
         exit('count_depth_increments() depth was set to less than one.\nQuitting..')
     
-    measurement_buckets = []
-    if depth == 1:
-        measurement_buckets = measurements
-    else:
-        measurement_buckets = list(list_to_buckets(measurements, depth))
+    if len(measurements) <= depth:
+        exit('not enough data to measure with given depth\nQuitting..')
 
-    if len(measurement_buckets) < 2:
-        exit('Measurement buckets length is too short, try lowering the depth parameter value..\nQuitting..')
-    
+    measurement_buckets = measurements
     depth_increase_count = 0
-    for index in range(1, len(measurement_buckets)):
-        previous = measurement_buckets[index-1]
-        current = measurement_buckets[index]
+    for index in range(1, len(measurement_buckets)+depth-1):
+        previous = sum(measurement_buckets[index-1:(index-1+depth)])
+        current = sum(measurement_buckets[index:(index+depth)])
 
         if previous < current:
             depth_increase_count += 1
@@ -51,8 +42,8 @@ def run():
     simple_tests()
     raw_measurements = read_file_to_list('measurements')
     measurements = list_to_int_list(raw_measurements)
-    depth_increase_count = count_depth_increments(measurements, depth=3)
-    print("Depth increase count was {}".format(depth_increase_count))
+    depth_increase_count = count_depth_increments(measurements, depth=DEPTH)
+    print("Depth increase count was {} at given depth of {}".format(depth_increase_count, DEPTH))
 
 if __name__ == '__main__':
     run()
