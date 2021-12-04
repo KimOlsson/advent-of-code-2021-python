8d0518a38c0f7bf4052ae2ac133127da9e1c7852
unknown <kkols94@gmail.com>
2021-12-04 21:49:31 +0200

    day 4: initial commit

---
+++ day4/bingo.py
@@ -0,0 +1,69 @@
+class Bingo:
+    """
+    This class let's you create a bingo board with your preferred width (row_length).
+    It let's you to update drawn bingo numbers and has functions to test if you
+    made a point vertically or horizontally.
+    
+    After a point has been made, drawn number hits are no longer updated, hence
+    score stays as is after winning.
+    """
+
+    def __init__(self, board: list[list[int]], row_length=5):
+        self.row_length = row_length
+        self.board = []
+        self.hits = []
+        self.board_score = 0
+        self.set_board(board)
+
+    def set_board(self, data: list[list[int]]):
+        if len(data) != self.row_length:
+            raise ValueError('Length of data was not {}'.format(self.row_length))
+        for row in data:
+            if len(row) != self.row_length:
+                raise ValueError('Length of a row was not {}'.format(self.row_length))
+            self.board.append(row)
+            self.board_score += sum(row)
+            self.hits.append([0]*self.row_length)
+
+    def is_hit(self, num: int):
+        if self.has_point():
+            return False
+        
+        for i, row in enumerate(self.board):
+            for j, col in enumerate(row):
+                if col == num:
+                    self.hits[i][j] = 1
+                    self.board_score -= num
+                    return True
+        return False
+
+    def has_horizontal_point(self):
+        for row in self.hits:
+            if sum(row) == self.row_length:
+                return True
+        return False
+
+    def has_vertical_point(self):
+        for i in range(self.row_length):
+            hit_sum = 0
+            for row in self.hits:
+                hit_sum += row[i]
+            
+            if hit_sum == self.row_length:
+                return True
+        return False
+
+    def has_point(self):
+        return self.has_vertical_point() or self.has_horizontal_point()
+
+    def get_board_score(self, last_number: int):
+        return self.board_score * last_number
+
+    def __repr__(self):
+        result = []
+        for i, row in enumerate(self.board):
+            line1 = "{:<2}{:>3}{:>3}{:>3}{:>3}".format(*row)
+            line2 = "{:<1}{:>3}{:>3}{:>3}{:>3}".format(*self.hits[i])
+            line3 = line1 + ' '*4 + line2
+            result.append(line3)
+        return '\n'.join(result)
---
+++ day4/giant_squid.py
@@ -0,0 +1,62 @@
+from bingo import Bingo
+
+ROW_LENGTH = 5
+
+def read_file_to_list(fname: str, encoding='utf-8') -> list[str]:
+    data = []
+    with open(fname, 'r', encoding=encoding) as file:
+        for line in file:
+            data.append(line.strip())
+    return data
+
+def str_to_int(value: str) -> int:
+    try:
+        return int(value)
+    except ValueError:
+        exit('You attempted to convert value {} to int\nQuitting..'.format(value))
+
+def parse_bingo_boards(raw_boards: list[str]) -> list[list[int]]:
+    boards_without_empty_lines = [x for x in raw_boards if len(x.strip())]
+    board = []
+    boards = []
+    for row in boards_without_empty_lines:
+        board_row = row.split()
+        board_row_int = [str_to_int(x) for x in board_row]
+        board.append(board_row_int)
+        
+        is_board_full = len(board) == ROW_LENGTH
+        if is_board_full:
+            boards.append(board)
+            board = []
+    return boards
+
+
+def draw_all_numbers(bingo_objects: list[Bingo], numbers: list[int]) -> tuple[Bingo, int]:
+    all_winners = []
+    for number in numbers:
+        for board in bingo_objects:
+            if not board.is_hit(number):
+                continue
+            if not board.has_point():
+                continue
+            all_winners.append((board, number))
+    return all_winners
+
+
+def run():
+    raw_bingo_data = read_file_to_list('bingo_data')
+    drawn_numbers = raw_bingo_data[0].split(',')
+    drawn_numbers_int = [str_to_int(x) for x in drawn_numbers]
+    raw_bingo_boards = raw_bingo_data[1:]
+    bingo_boards = parse_bingo_boards(raw_bingo_boards)
+    bingo_objects = [Bingo(x, ROW_LENGTH) for x in bingo_boards]
+
+    all_winners_in_order = draw_all_numbers(bingo_objects, drawn_numbers_int)
+    winner, winner_with_number = all_winners_in_order[0]
+    print(f"\nWinner was board:\n\n{str(winner)}\n\nWith score {winner.get_board_score(winner_with_number)}")
+
+    last_winner, last_winner_with_number = all_winners_in_order[-1]
+    print(f"\nLast to win board:\n\n{str(last_winner)}\n\nWith score {last_winner.get_board_score(last_winner_with_number)}")
+
+if __name__ == '__main__':
+    run()
