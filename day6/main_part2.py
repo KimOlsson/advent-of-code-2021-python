338843a6e00f00c7238d574dbe505a0590c02a24
unknown <kkols94@gmail.com>
2021-12-07 23:55:03 +0200

    day 6: part2, just saving this mess since it might just work

--- day6/main_part2.py
+++ day6/main_part2.py
@@ -24,29 +24,47 @@ def firstborn(time_left: int):
     return count
 
 def lanternfish_single_parent_family_tree(lanternfish, days):
-    count = 1
+    count = 0
     if days < lanternfish+1:
         return count
     time_left = days-(lanternfish+1)
+    #count = 1
     count += firstborn(time_left)
     count += lanternfish_single_parent_family_tree(6, time_left)
     return count
 
 
-def lanternfish_full_family_tree_size(lanternfishes: list[int], days=18):
-    count = len(lanternfishes)  # Taking in the account parents themselves
-    for parent in lanternfishes:
+def lanternfish_full_family_tree_size(lanternfishes: list[int], days=12):
+    count_in_order = []
+    for i, parent in enumerate(lanternfishes):
+        count = 0
+        print(f"Starting parent {i}")
         # Each fish has internal timer, lets calculate beginning of full cycle
         time_left = days-(parent+1)   # parent at day 3 should become 3..2..1..0..6, 4 steps.
+        count += firstborn(time_left) # Family tree of the offspring
+        print(f"\tParent {i} firstborn family tree counted..")
         count += lanternfish_single_parent_family_tree(6, time_left)
+        print(f"parent {i} familytree finished\n")
+        count_in_order.append(count)
+        #return count
     return count
 
 def run():
     raw_timers = read_file_to_list('data')
     timers = raw_timers[0].split(',')
     timers_int = [str_to_int(x) for x in timers]
-    offsprings = lanternfish_full_family_tree_size([3,4,3,1,2])
-    #offsprings = amount_of_offsprings(timers_int)
+    #offsprings = lanternfish_full_family_tree_size([3,4,3,1,2])
+    count_of_timers = {}
+    for unique_timer in timers_int:
+        if unique_timer not in count_of_timers:
+            count_of_timers[unique_timer] = 1
+        count_of_timers[unique_timer] += 1
+    print(count_of_timers)
+
+    new_timers = list(set(timers_int))
+    print(new_timers)
+    offsprings = lanternfish_full_family_tree_size(new_timers)
+    offsprings += len(timers_int)
     print(offsprings)
     
 
